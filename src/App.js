07126875cbdffd424effc336fa2e879bb7078c3d
Debosmita Roy <debosmita456@gmail.com>
2023-07-08 05:10:35 +0530

    first_commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Header from './Header'
+import Content from './Content';
+import Footer from './Footer'
+
+function App() {
+  const name = "mishtu"
+  
+  return (
+    <div className="App">
+      <Header/>
+      <Content/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
