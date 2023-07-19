99ad1f9151b47bc62a74fbdbea883b78cc3e549b
Debosmita Roy <debosmita456@gmail.com>
2023-07-19 13:03:08 +0530

    Groceries list

--- src/Content.js
+++ src/Content.js
@@ -6,7 +6,7 @@ const Content = ({items, handleCheck, handleDelete}) => {
     
 
     return(
-        <main>
+        <>
             {items.length ?(
                 <ItemList
                     items= {items}
@@ -17,7 +17,7 @@ const Content = ({items, handleCheck, handleDelete}) => {
             
             
 
-        </main>
+        </>
     )
 }
 
